633baf2ed6761331c9a24041a8c43dfa769bbf76
Alexander frenzel <alex@relatedworks.com>
2014-08-19 12:27:34 +0200

    added sourcemaps to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp'),
     sass = require("gulp-sass"),
     coffee = require("gulp-coffee"),
+    sourcemaps = require("gulp-sourcemaps"),
     connect = require('gulp-connect');
 
 // task
 gulp.task('coffee', function() {
     gulp.src('src/jquery-select-areas.coffee')
+    .pipe(sourcemaps.init())
     .pipe(coffee())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist'))
 });
 
@@ -36,4 +39,4 @@ gulp.task('serve', function () {
 
 gulp.task('build', ['coffee', 'sass']);
 
-gulp.task('default', ['watch', 'serve']);
+gulp.task('default', ['build', 'watch', 'serve']);
